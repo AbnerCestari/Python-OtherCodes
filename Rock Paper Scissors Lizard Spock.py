5753eb09ea5bfdccf5ce19e832605072686fe2a0
David Jovino <92277598+DavidJovino@users.noreply.github.com>
2023-02-23 10:18:00 -0300

    Changed if statement to functions

--- Rock Paper Scissors Lizard Spock.py
+++ Rock Paper Scissors Lizard Spock.py
@@ -13,137 +13,47 @@
 
 import random
 
-def RPCLS():
-    choices = ["Rock","Paper","Scissors","Lizard","Spock"]
+# Definindo escolhas
+choices = {"rock": {"scissors", "lizard"},
+           "paper": {"rock", "spock"},
+           "scissors": {"paper", "lizard"},
+           "lizard": {"paper", "spock"},
+           "spock": {"rock", "scissors"}}
 
-    computer = random.choice(choices)
-    player = None
+win_verbs = {"rock": {"scissors": "smashes", "lizard": "crushes"},
+             "paper": {"rock": "covers", "spock": "disproves"},
+             "scissors": {"paper": "cuts", "lizard": "decapitates"},
+             "lizard": {"paper": "eats", "spock": "poisons"},
+             "spock": {"rock": "vaporizes", "scissors": "smashes"}}
 
-    print ("Let's play!")
+# Função pegar a escolha do usuario
+def get_user_choice():
+    while True:
+        choice = input("Let's play!" + "\n" + "Rock, Paper, Scissors, Lizard or Spock?" + "  ").lower()
+        if choice in choices:
+            return choice
+        else:
+            print("Invalid input!")
 
-    while player not in choices:
-        player = input("Rock, Paper, Scissors, Lizard or Spock? ").capitalize()
-    
-    if player == computer:
-        print ("Computer: ", computer)
-        print ("Player: ", player)
-        print ("Tie!!")
+# Função pegar escolha do computador
+def get_computer_choice():
+    return random.choice(list(choices.keys()))
 
-    elif player == "Rock":
-
-        if computer == "Paper":
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Paper covers rock. You lose!!")
-
-        if computer == "Scissors": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Rock crushes scissors. You win!!!")
-
-        if computer == "Lizard": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Rock crushes lizard. You win!!!")
-
-        if computer == "Spock": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Spock vaporizes rock. You lose!!!")
-
-    elif player == "Paper": 
-        
-        if computer == "Rock":
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Paper covers rock. You win!!!")
-
-        if computer == "Scissors":
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Scissors cuts paper. You lose!!")
-
-        if computer == "Lizard": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Lizard eats paper. You lose!!!")
-
-        if computer == "Spock": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Paper disproves Spock. You win!!!")
-
-    elif player == "Scissors":
-
-        if computer == "Rock":
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Rock crushes scissors. You lose!!")
-
-        if computer == "Paper":
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Scissors cuts paper. You win!!!")
-
-        if computer == "Lizard": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Scissors decapitates lizard. You win!!!")
-
-        if computer == "Spock": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Spock smashes scissors. You lose!!!")
-    
-    elif player == "Lizard":
-
-        if computer == "Rock":
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Rock crushes lizard. You lose!!")
-
-        if computer == "Paper": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Lizard eats paper. You win!!!")
-        
-        if computer == "Scissors": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Scissors decapitates Lizard. You lose!!!")
-
-        if computer == "Spock": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Lizard poisons Spock. You win!!!")
-
-    elif player == "Spock":
-
-        if computer == "Rock": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Spock vaporizes rock. You win!!!")
-
-        if computer == "Paper":
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Paper disproves Spock. You lose!!")
-
-        if computer == "Scissors": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Spock smashes scissors. You win!!!")
-
-        if computer == "Lizard": 
-            print ("Computer: ", computer)
-            print ("Player: ", player)
-            print ("Lizard poisons Spock. You lose!!!")
-
-while True:
-    RPCLS()
-    replay = input ("Do you wish to play again? Y/N\n").upper()
-    if replay == "N":
-        break
+# Função para determinar o vencedor
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "Tie"
+    elif computer_choice in choices[user_choice]:
+        return user_choice.capitalize() + " " + win_verbs[user_choice][computer_choice] + " " + computer_choice + "." + " " + " You win!!!!"
     else:
-        print()
-        continue
+        return computer_choice.capitalize() + " " + win_verbs[computer_choice][user_choice] + " " + user_choice.capitalize() + "." + " " + " You lose!!"
+
+# loop
+while True:
+    user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
+    print(f"You chose {user_choice} and computer chose {computer_choice}.")
+    print(determine_winner(user_choice, computer_choice))
+    play_again = input("Do you wish to play again? Y/N ").lower()
+    if play_again != "y":
+        break
